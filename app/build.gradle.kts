0ec626b815810517292b1452df96a5ac4e34c800
Germán <germanskip234@gmail.com>
2026-06-05 18:12:57 -0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "2.0.0"
+}
+
+android {
+    namespace = "com.gonzalez.tupmundial2026"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.gonzalez.tupmundial2026"
+        minSdk = 29
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    // Retrofit core
+    implementation(libs.retrofit)
+// Convertidor JSON (Gson)
+    implementation(libs.converter.gson)
+    // 1. Librería base de Kotlinx Serialization (JSON)
+    implementation(libs.kotlinx.serialization.json)
+    // 2. Convertidor oficial de Retrofit para Kotlinx Serialization (Square)
+    // Esta es la implementación mantenida por los creadores de Retrofit
+    implementation(libs.converter.kotlinx.serialization)
+    // Implementación principal (incluye dependencias de Android)
+    implementation(libs.androidx.datastore.preferences)
+    // Versión estable recomendada (Mayo 2026)
+    implementation(libs.androidx.navigation.compose)
+
+}

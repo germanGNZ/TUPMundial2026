4849328958aab820ef58ebfe9f1428893b43cbe3
ulimax420640 <ulimax420640@gmail.com>
2026-06-11 15:44:14 +0000

    Initial commit - Proyecto Hello Android

--- app/src/main/java/com/gonzalez/tupmundial2026/models/Navigation.kt
+++ app/src/main/java/com/gonzalez/tupmundial2026/models/Navigation.kt
@@ -1,2 +1,20 @@
 package com.gonzalez.tupmundial2026.models
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.gonzalez.tupmundial2026.ui.PartidosScreen
+import com.gonzalez.tupmundial2026.viewModel.MundialViewModel
+
+@Composable
+fun AppNavigation(modifier: Modifier = Modifier, viewModel: MundialViewModel) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "partidos", modifier = modifier) {
+        composable("partidos") {
+            PartidosScreen(viewModel = viewModel)
+        }
+        // Agrega más pantallas (detalle de partido, etc.)
+    }
+}
